9b597b0221f8b920644854e82b0d2fea0bc6f7af
negitorogithub <unifarproject35@gmail.com>
2018-12-08 19:36:03 +0900

    GetButton

--- app/src/main/java/unifar/unifar/messagebottle/ui/main/MainFragment.kt
+++ app/src/main/java/unifar/unifar/messagebottle/ui/main/MainFragment.kt
@@ -1,10 +1,9 @@
 package unifar.unifar.messagebottle.ui.main
 
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.os.Debug
 import android.support.v4.app.Fragment
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,6 +30,7 @@ class MainFragment : Fragment() {
         val view = inflater.inflate(R.layout.main_fragment, container, false)
         val submitButton = view.findViewById(R.id.submitButton) as Button
         val messageEditText = view.findViewById(R.id.messageEditText) as EditText
+        val getButton = view.findViewById(R.id.getButton) as Button
 
         submitButton.setOnClickListener {  _ ->
             //まずカウンター取得
@@ -66,8 +66,46 @@ class MainFragment : Fragment() {
                                     }
                             }
                     }
+                }
+        }
 
+        getButton.setOnClickListener { _ ->
+            //まずカウンター取得
+            db.collection("counters")
+                .document("content")
+                .get()
+                //次に内容取得
+                .addOnCompleteListener { task1 ->
+                    val data1 = task1.result?.data
+                    val dbSize = data1?.get("size")?.toString()?.toInt()
+                    val cursor = data1?.get("cursor")?.toString()?.toInt()
+                    Log.d("myapp", "cursor:$cursor")
 
+                    cursor?.let { _ ->
+                        val nextCursor = cursor + 1
+                        db.collection("messages")
+                            .document("$cursor")
+                            .get()
+                            .addOnCompleteListener { task2 ->
+                                val data2 = task2.result?.data
+                                val content = data2?.get("message")?.toString()
+                                Toast.makeText(activity, "$content", Toast.LENGTH_LONG).apply {setGravity(Gravity.CENTER,0,0)}.show()
+                            }
+                            //最後にカウンター更新
+                            .addOnCompleteListener { _ ->
+                                dbSize?.let {
+                                    if (nextCursor <= dbSize)
+                                        db.collection("counters")
+                                            .document("content")
+                                            .update("cursor", nextCursor)
+                                            .addOnSuccessListener { _ ->
+                                                Toast.makeText(activity, "cursor was updated to!:$nextCursor ", Toast.LENGTH_LONG).show()
+                                            }
+                                    else
+                                        Toast.makeText(activity, "cursor is still!:$cursor ", Toast.LENGTH_LONG).show()
+                                }
+                            }
+                    }
                 }
         }
         return view
@@ -78,6 +116,7 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         db = FirebaseFirestore.getInstance()
+
     }
 
 }
